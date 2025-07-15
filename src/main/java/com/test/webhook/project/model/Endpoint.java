11228203c798412b7f6504f501b657016f13f3f4
Kanav Rohatgi <kanavr@gmail.com>
2025-07-15 14:20:22 +0530

    create incomingReq entity and mapped the relatipnships

--- src/main/java/com/test/webhook/project/model/Endpoint.java
+++ src/main/java/com/test/webhook/project/model/Endpoint.java
@@ -1,15 +1,18 @@
 package com.test.webhook.project.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -44,6 +47,9 @@ public class Endpoint {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "endpoint", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<IncomingRequest> incomingRequests;
 
 
 }
---
+++ src/main/java/com/test/webhook/project/model/IncomingRequest.java
@@ -0,0 +1,46 @@
+package com.test.webhook.project.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class IncomingRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long requestId;
+    
+    private String method;
+
+    @Lob
+    private String headers;
+    @Lob
+    private String body;
+    @Lob
+    private String queryParams;
+    @Lob
+    private String path;
+    
+    private LocalDateTime receivedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "endpoint_id")
+    private Endpoint endpoint;
+
+}
